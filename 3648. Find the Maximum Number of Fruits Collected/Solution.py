f4bc7fef3a2f2e77505370fdd7754bbe5a4f0045
Kishan Dev <118914499+kishandev2509@users.noreply.github.com>
2025-08-08 17:48:27 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3648
          Language: Python3
          Runtime: 2162 ms
          Runtime percentile: 16.13
          Memory: 107.08 MB
          Memory percentile: 32.26

--- 3648. Find the Maximum Number of Fruits Collected/Solution.py
+++ 3648. Find the Maximum Number of Fruits Collected/Solution.py
@@ -1,37 +1,47 @@
 class Solution:
     def maxCollectedFruits(self, fruits: List[List[int]]) -> int:
         n = len(fruits)
-        dp = [[-1]*n for _ in range(n)]
-        def count_fruits(r, c, m, next_moves):
-            if r == c == n-1 :
-                return 0
-            if r == c or m == 0:
-                dp[r][c] = float("-inf")
-                return float("-inf")
-            if dp[r][c] != -1:
-                return dp[r][c]
-            max_fruits = -1
-            for nm in next_moves:
-                nr = r + nm[0]
-                nc = c + nm[1]
-                if not 0 <= nr < n > nc >= 0:
-                    continue
-                max_fruits = max(max_fruits,count_fruits(nr,nc,m-1,next_moves))
-            if max_fruits == -1:
-                dp[r][c] = float("-inf")
-                return float("-inf")
-            dp[r][c] =  max_fruits + fruits[r][c]
-            return max_fruits + fruits[r][c]
+        dp: list[list[int]] = [[-1] * n for _ in range(n)]
+
+        def count_child2_fruits(row, col, next_moves):
+            dp[n - 1][n - 1] = 0
+            for r in range(n - 1, 0, -1):
+                for c in range(n - 1, r - 1, -1):
+                    for nm in next_moves:
+                        nr = r - nm[0]
+                        nc = c - nm[1]
+                        if not 0 <= nr < n > nc >= 0:
+                            continue
+                        if nr == nc:
+                            dp[nr][nc] = 0
+                            continue
+                        dp[nr][nc] = max(dp[nr][nc], dp[r][c] + fruits[nr][nc])
+            return dp[row][col]
+
+        def count_child3_fruits(row, col, next_moves):
+            dp[n - 1][n - 1] = 0
+            for c in range(n - 1, 0, -1):
+                for r in range(n - 1, c - 1, -1):
+                    for nm in next_moves:
+                        nr = r - nm[0]
+                        nc = c - nm[1]
+                        if not 0 <= nr < n > nc >= 0:
+                            continue
+                        if nr == nc:
+                            dp[nr][nc] = 0
+                            continue
+                        dp[nr][nc] = max(dp[nr][nc], dp[r][c] + fruits[nr][nc])
+            return dp[row][col]
+
         child1 = 0
         for i in range(n):
             child1 = child1 + fruits[i][i]
-            
-        child2_moves = [(1,-1),(1,0),(1,1)]
-        child3_moves = [(-1,1),(0,1),(1,1)]
-        
-        child2 = count_fruits(0,n-1,n-1,child2_moves)
-        dp = [[-1]*(n) for _ in range(n)]
-        child3 = count_fruits(n-1,0,n-1,child3_moves)
-        
+
+        child2_moves = [(1, -1), (1, 0), (1, 1)]
+        child3_moves = [(-1, 1), (0, 1), (1, 1)]
+
+        child2 = count_child2_fruits(0, n - 1, child2_moves)
+        child3 = count_child3_fruits(n - 1, 0, child3_moves)
+
         max_fruits = child1 + child2 + child3
-        return int(max_fruits)
+        return max_fruits
