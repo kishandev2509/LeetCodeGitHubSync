eef3666dfd2de00a394a2b000983d2d50ff18228
Kishan Dev <118914499+kishandev2509@users.noreply.github.com>
2025-08-08 18:47:56 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 826
          Language: Python3
          Runtime: 7 ms
          Runtime percentile: 93.07
          Memory: 20.328 MB
          Memory percentile: 18.81

--- 826. Soup Servings/Solution.py
+++ 826. Soup Servings/Solution.py
@@ -1,12 +1,13 @@
+from functools import lru_cache
+
+
 class Solution:
     def soupServings(self, n: int) -> float:
         if n > 4800:
-            return 1
-        memo = {}
+            return 1.0
 
+        @lru_cache(None)
         def solve(a, b):
-            if (a, b) in memo:
-                return memo[(a, b)]
             if a <= 0 and b <= 0:
                 return 0.5
             if a <= 0:
@@ -14,6 +15,7 @@ class Solution:
             if b <= 0:
                 return 0.0
             prob = 0.25 * (solve(a - 100, b) + solve(a - 75, b - 25) + solve(a - 50, b - 50) + solve(a - 25, b - 75))
-            memo[(a, b)] = prob
             return prob
-        return solve(n,n)
+
+        return solve(n, n)
+
